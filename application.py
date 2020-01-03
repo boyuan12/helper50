548e86870292f6eb9874cb2b157cb4dbffc5ec57
boyuan12 <50649079+boyuan12@users.noreply.github.com>
2020-01-03 08:02:10 -0800

    Delete application.py

--- application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def default():
-    return "<h1>Build Successful</h1>"
-
-# db link: postgres://rubeaiwxtkzapn:c306da7057d574936f76017816e64df6b880a662c8b42b7b94c12b5183227b49@ec2-107-22-224-154.compute-1.amazonaws.com:5432/degjcsteagd0gc
