eca4a9a00bef98cf214bf0f28353d7ccdf3fb68d
[Boyuan Liu] <50649079+boyuan12@users.noreply.github.com>
2020-01-03 07:34:39 -0800

    Deploy

--- application.py
+++ application.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def default():
     return "<h1>Build Successful</h1>"
 
-# db link: postgres://qpyazjzqokyzax:ed7333462683a13b5b70e32f451378d79d95dfe41a18d97c9adf983fd3cab49b@ec2-54-225-106-93.compute-1.amazonaws.com:5432/d658ji5p1npm9e
+# db link: postgres://rubeaiwxtkzapn:c306da7057d574936f76017816e64df6b880a662c8b42b7b94c12b5183227b49@ec2-107-22-224-154.compute-1.amazonaws.com:5432/degjcsteagd0gc
